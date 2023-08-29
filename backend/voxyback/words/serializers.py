8e64ea2d342ddc778147848280156f1a427dd75d
Lucas Silva <lucasarasil1@gmail.com>
2023-08-29 12:54:53 -0300

    Improve test cases and add new requirements

--- backend/voxyback/words/serializers.py
+++ backend/voxyback/words/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import serializers
 from words.models import TextSearch
 
@@ -6,8 +7,14 @@ class TextSerchSerializer(serializers.ModelSerializer):
         model = TextSearch
         fields = ['id', 'text', 'wordsQtd']
         read_only_fields = ['wordsQtd']
+
+    def clean_text(self, text):
+        return str(text).lower()
+
+    def filter_special_chars(self, text):
+        return re.match('^[a-zA-Z0-9]*$',text)
     
     def create(self, validated_data):
         dataCopy = validated_data.copy()
-        dataCopy["wordsQtd"] = len(dataCopy['text'].split())
+        dataCopy["wordsQtd"] = len(set(map(self.clean_text, filter(self.filter_special_chars,dataCopy['text'].split()))))
         return super().create(dataCopy)
